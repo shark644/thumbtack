62b32c27e6a96dae1a14acc7ae86438c2bc511b4
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2022-07-16 04:34:11 +0600

    firebase added

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnQOpGdrJ9U0SknG1MMIXIW5Iq-eKkUoo",
+  authDomain: "thumbtack-a6cbb.firebaseapp.com",
+  projectId: "thumbtack-a6cbb",
+  storageBucket: "thumbtack-a6cbb.appspot.com",
+  messagingSenderId: "262636818785",
+  appId: "1:262636818785:web:ba73ce121451363830b520",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import "./style/style.scss";
 import Alpine from "alpinejs";
+import app from "./firebase-config";
 
 window.Alpine = Alpine;
 
