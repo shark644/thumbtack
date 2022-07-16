701b3da5cf7bb1891da347e154bf4fd47488fbc9
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2022-07-17 01:21:10 +0600

    - hero section added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    fontFamily: {
+      mark: "Mark, Avenir, Helvetica, Arial, sans-serif",
+      "mark-bold": "Mark bold, Avenir, Helvetica, Arial, sans-serif",
+      "mark-heavy": "Mark heavy, Avenir, Helvetica, Arial, sans-serif",
+    },
     extend: {
       colors: {
         black: "#2f3033",
