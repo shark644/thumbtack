fd6e5bb231c7052727beb11662addd89d567aeb8
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2022-08-06 12:51:43 +0600

    pro page till get more jobs slider

--- pro/splide.js
+++ pro/splide.js
@@ -32,3 +32,31 @@ const defaultOptions = {
 };
 
 new Splide("#prosReviewSlider", defaultOptions).mount();
+new Splide("#getMoreJobsSlider", {
+  arrows: true,
+  perPage: 3,
+  perMove: 3,
+  gap: 16,
+  classes: {
+    prev: "splide__arrow--prev splide-prev-btn",
+    next: "splide__arrow--next splide-next-btn",
+
+    // Add classes for pagination.
+    pagination: "splide__pagination", // container
+    page: "splide__pagination__page hidden", // each button
+  },
+  breakpoints: {
+    1024: {
+      perPage: 2,
+    },
+    640: {
+      perPage: 1,
+      arrows: false,
+      drag: "free",
+      pagination: false,
+      padding: {
+        right: 80,
+      },
+    },
+  },
+}).mount();
